53af724fd7947797e5cf8dc453dc91754fd8945d
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-04-02 14:09:39 -0400

    Completed Guided Demo

--- api/server.js
+++ api/server.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const sessionConfig = require("../auth/session-config.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
